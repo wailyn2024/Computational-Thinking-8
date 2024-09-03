f3611e714efe55fe1271eced9c8c99d0821c0217
Blakely12 <blakelykriegler@seattleacademy.org>
2024-09-03 16:24:14 +0000

    setup

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -6,16 +6,16 @@ stage = StageClass()
 ###############################################
 
 
-stage.set_background("fall")
-mySprite = codesters.Sprite("cardinal")
-mySprite.say("Good job finding me!")
+stage.set_background("summer")
+mySprite = codesters.Sprite("fish")
+mySprite.say("Olaf on top")
 
 
 print("Welcome to CT8!")
 print("Great job correctly setting up this codespace")
 print("We will be using this site for the majority of the projects for this class")
 print("Make sure you bookmark this page using the STAR icon in the top right corner of your browser")
-input("\n---Press Enter when you have bookmarked the page---\n")
+
 
 
 print("\n\n")
@@ -26,3 +26,4 @@ print("\t2: move your mouse over the words in blue that start with https://")
 print("\t3: a few icons should appear - click the globe")
 print("\t4: a new tab will open - click CONNECT")
 print("\n\nWhen you have found the CARDINAL, click here, then use CTRL C to end the program\n\n")
+print("This is the last new instruction")

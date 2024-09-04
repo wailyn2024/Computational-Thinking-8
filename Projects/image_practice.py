1f5d3ae8b81d4c2a97d093f0ea885da34f8a587b
Jamari716 <Jamariross@seattleacademy.org>
2024-09-04 19:16:48 +0000

    image practice program

---
+++ Projects/image_practice.py
@@ -0,0 +1,15 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
+
+
+stage.set_background("summer")
+sprite1 = codesters.Sprite("Rickenbacker-1-6.png",100,0)
+sprite1.say("hello")
+sprite2 = codesters.sprite("cardinal",-100,0)
+sprite2.say("hi")

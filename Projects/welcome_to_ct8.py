99f7de140d7f6ceae033b1f6f89795f832049c1d
NWarikoo-Academy <neelwarikoo@seattleacademy.org>
2024-09-04 19:27:28 +0000

    setup

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -5,17 +5,15 @@ from codesters import StageClass
 stage = StageClass()
 ###############################################
 
-
-stage.set_background("fall")
-mySprite = codesters.Sprite("cardinal")
-mySprite.say("Good job finding me!")
+stage.set_background("winter")
+mySprite = codesters.Sprite("character1") 
+mySprite.say("Hello!")
 
 
 print("Welcome to CT8!")
-print("Great job correctly setting up this codespace")
+print("Great job correctly setting up this code space")
 print("We will be using this site for the majority of the projects for this class")
 print("Make sure you bookmark this page using the STAR icon in the top right corner of your browser")
-input("\n---Press Enter when you have bookmarked the page---\n")
 
 
 print("\n\n")

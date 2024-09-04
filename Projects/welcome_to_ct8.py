25ac77f9d6e9beb09318bc9a6aba2a0c8dc2c280
NWarikoo-Academy <neelwarikoo@seattleacademy.org>
2024-09-04 19:29:02 +0000

    test2

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -14,3 +14,5 @@ print("Welcome to CT8!")
 print("Great job correctly setting up this code space")
 print("We will be using this site for the majority of the projects for this class")
 print("Make sure you bookmark this page using the STAR icon in the top right corner of your browser")
+
+# test submission

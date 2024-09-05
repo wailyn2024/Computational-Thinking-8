fc241c0b2562e321161df8ab7eabbb618cc2582d
braylen-333 <braylenbrooks@seattleacademy.org>
2024-09-05 21:43:22 +0000

    project 1 submission

---
+++ Projects/image-practice.py
@@ -0,0 +1,22 @@
+
+
+
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
+stage.set_background("fall")
+mySprite = codesters.Sprite("cardinal")
+mySprite.say("Good job finding me!")
+
+
+stage.set_background("fall")
+mySprite2 = codesters.Sprite("cat")
+mySprite2.say("hi")
+mySprite2.set_size (0.30)
+mySprite2.set_bottom
+

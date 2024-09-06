85c8ddcf102b400a2b6a00e02caa03b84416c594
Blakely12 <blakelykriegler@seattleacademy.org>
2024-09-06 20:34:21 +0000

    fix 9/6

---
+++ Projects/image_testing.py
@@ -0,0 +1,17 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
+stage.set_background("summer")
+mySprite = codesters.Sprite("dylanhoff")
+
+
+
+print("Welcome to CT8!")
+print("Great job correctly setting up this codespace")
+mySprite2 = codesters.Sprite("corgi")
+mySprite2.say("Hi")

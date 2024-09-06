e493fa1d8fe309d75b259e11279036f8e20fbc01
namlek1 <oscarkelman@seattleacademy.org>
2024-09-06 19:01:57 +0000

    project - lost images :(

---
+++ Projects/project.py
@@ -0,0 +1,19 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+stage.set_background ("winter")
+
+q1 = codesters.Square (100, 100, 200, "blue")
+q1 = codesters.Square (-100, 100, 200, "red")
+q1 = codesters.Square (-100, -100, 200, "green")
+q1 = codesters.Square (100, -100, 200,"yellow")
+
+s1 = codesters.Sprite ("download", 100, 100)
+s2 = codesters.Sprite ('soccer', -50, -20)
+s3 = codesters.Sprite ('seattle', 100, -100)
+s4 = codesters.Sprite ('bob', -100, 100)
+s4.set_size(0.5)

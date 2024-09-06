4aaaae2e1ce78cc75ec2983b0ed80716eefc057e
linkruby <nateshlarkin@seattleacademy.org>
2024-09-06 18:55:01 +0000

    9/5

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -12,6 +12,4 @@ mySprite.say("I like pancakes!")
 mySprite.set_size(1.5)
 
 
-print("\n\nWhen you have found the CARDINAL, click here, then use CTRL C to end the program\n\n")
-print(" this is the last instruction")
 mySprite2 = codesters.Sprite("baseball",-250,250)

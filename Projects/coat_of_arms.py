9befba0b78d89efd91ee9f05d17461f037941617
aaditpurohit <aaditpurohit@seattleacademy.org>
2024-09-09 15:57:51 +0000

    setp

---
+++ Projects/coat_of_arms.py
@@ -0,0 +1,10 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
+square1 = codesters.Square(100,100,200,'Blue')
+square2 = codesters.Square(-100,100,200, 'Red')

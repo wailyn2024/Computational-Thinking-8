6f059db61d2a79a0b43e16a722c3c609ec6baaf5
sheydaharvey <sheydaharvey@seattleacademy.org>
2024-09-09 16:12:48 +0000

    setupjk

---
+++ Projects/coat_of_arms.py
@@ -0,0 +1,15 @@
+
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
+stage.set_background("winter")
+mySprite = codesters.Sprite("cleangirl")
+mySprite.set_size(0.70)
+mySprite.say (" your such a clean girl")
+print("this is the new last instruction")
+Mydreamnails= codesters.Sprite("fish",-150,150)

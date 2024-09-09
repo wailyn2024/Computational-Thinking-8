2e6aeeed6adfb9238f1632b7e01eaf0f7f21223c
BradyBBB <bradygalante@seattleacademy.org>
2024-09-09 16:00:31 +0000

    setup2

---
+++ Projects/coat_of_arms.py
@@ -0,0 +1,8 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+stage.set_background("winter")
+            

b540eed2f6589b089720845881107f2307f536ac
lidapida <lydiaryvkin@seattleacademy.org>
2024-09-09 16:18:16 +0000

    setupjk

---
+++ Projects/coat_of_arms.py
@@ -0,0 +1,14 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+stage.set_background("fall")
+
+q1=codesters.Square(100, 100, 200, 'pink')
+s1= codesters.Sprite ("cardinal" ,100,100)
+s2=codesters.Sprite("cat",-100,-100)
+s2.set_size(0.5)
+message1= codesters.Text("hi",0,220,"light blue")
+message2= codesters.Text("hello" ,0,-220,"blue")

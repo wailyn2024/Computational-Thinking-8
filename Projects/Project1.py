cecfbcdd4fa1970a9b35d47dcc87a8fe062d8b1f
NWarikoo-Academy <neelwarikoo@seattleacademy.org>
2024-09-10 19:03:11 +0000

    Project 1 submission, neel warikoo

--- Projects/Project1.py
+++ Projects/Project1.py
@@ -5,18 +5,19 @@ from codesters import StageClass
 stage = StageClass()
 ###############################################
 
-stage.set_background("winter")
+stage.set_background("park")
 
 q1 = codesters.Square(100, 100, 200, 'blue')
 q2 = codesters.Square(-100, 100, 200, 'yellow')
 q3 = codesters.Square(-100, -100, 200, 'red')
 q4 = codesters.Square(100, -100, 200, 'green')
 
-s1 = codesters.Sprite("dnd", 100, 100)
-s2 = codesters.Sprite("drift", -100, -100)
-s2.set_size(0.5)
+s1 = codesters.Sprite("DND", 100, 100)
+s1.set_size(0.4)
+s2 = codesters.Sprite("Drift", -100, -100)
+s2.set_size(0.3)
 s3 = codesters.Sprite("family", 100, -100)
-s3.set_size(0.5)
+s3.set_size(0.6)
 s4 = codesters.Sprite("cardinal", -100, 100)
 s2.set_size(0.1)
 

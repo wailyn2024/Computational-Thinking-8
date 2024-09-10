2d6c83302953a8d841439898d350d58899f647e8
Astrojam8 <liamkudoking@seattleacademy.org>
2024-09-10 19:03:29 +0000

    project 1 submission

--- Projects/coat_of_arms.py
+++ Projects/coat_of_arms.py
@@ -5,19 +5,22 @@ stage = StageClass()
 ########################
 
 
-stage.set_background("underwater")
+stage.set_background("forest")
 
 q1 = codesters.Square(100,100,200, "green")
-q2 = codesters.Square(-100,100,200,"green")
-q3 = codesters.Square(-100,-100,200, "brown")
+q2 = codesters.Square(-100,100,200,"grey")
+q3 = codesters.Square(-100,-100,200, "beige")
 q4 = codesters.Square(100,-100,200,"black")
+q5 = codesters.TriangleRight(100,100,200,200,"orange")
 
 
-s1 = codesters.Sprite("bat",100,100)
-s2 = codesters.Sprite("cardinal", -100,-100)
+s1 = codesters.Sprite("toucan",100,100)
+s1.set_size(.2)
+s2 = codesters.Sprite("cat2", -100,-100)
 s3 = codesters.Sprite("guitar", 100, -100)
-s3.set_size(0.25)
-s4 = codesters.Sprite("fish", -100, 100)
+s3.set_size(0.18)
+s4 = codesters.Sprite("book", -100, 100)
+s4.set_size(.35)
 
-message1 = codesters.Text("random message",0, 220,"green")
-message2 = codesters.Text("blah blah blah",0,-220,"black")
+message1 = codesters.Text("Liam Kudo-King",0, 220,"white")
+message2 = codesters.Text("blah blah blah",0,-220,"white")

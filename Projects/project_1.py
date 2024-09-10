3ec6b751cbef2b6be4d896690323a6ce3b191bab
Jamari716 <Jamariross@seattleacademy.org>
2024-09-10 19:14:10 +0000

    setupjk

--- Projects/project_1.py
+++ Projects/project_1.py
@@ -6,12 +6,15 @@ stage = StageClass()
 ###############################################
 
 
+
 stage.set_background("rainstorm")
-sprite1 = codesters.sprite("drums",100,100)
+
+sprite1 = codesters.sprite("drum",100,100)
 sprite2 = codesters.sprite("sailboat",100,-100)
-sprite3 = codesters.sprite("rickenbacker-1-6",-100,100)
+sprite3 = codesters.sprite("bass",-100,100)
 sprite4 = codesters.sprite("seattle",-100,-100)
-square1 = codesters.Square("red",100,-100)
-square2 = codesters.Square("purple",100,100) 
-square3 = codesters.Square("white",-100,100)
-square4 = codesters.Square("black",-100,-100)
+
+square1 = codesters.sprite("red",100,-100)
+square2 = codesters.sprite("purple",100,100) 
+square3 = codesters.sprite("white",-100,100)
+square4 = codesters.sprite("black",-100,-100)

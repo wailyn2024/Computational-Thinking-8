a8a469e509c11137abde4ca150dc4cbc2fd8eb8e
H1ImL0st <divitgupta@seattleacademy.org>
2024-09-10 19:12:18 +0000

    Project 1 Update

--- Projects/Coat_of_arms.py
+++ Projects/Coat_of_arms.py
@@ -4,12 +4,18 @@ import codesters
 from codesters import StageClass
 stage = StageClass()
 ###############################################
-stage.set_background("mountain")
+stage.set_background("City")
+
 quart1 = codesters.Square(100, 100, 200,'DarkOrchid')
 quart2 = codesters.Square(-100, 100, 200,'Goldenrod')
-quart3 = codesters.Triangle(100, -100, 200,'LightCoral')
-quart4 = codesters.Triangle(-100, -100, 200,'SeaGreen')
-quart4.turn_right(45)
+quart3 = codesters.Triangle(100, -59, 200,'LightCoral')
+quart4 = codesters.Triangle(-100, -59, 200,'SeaGreen')
+quart5 = codesters.Triangle(0, -119, 200,'Navy')
+
+quart4.turn_right(60)
+quart3.turn_right(-60)
+
 message1 = codesters.Text("Divit J Gupta",0,220,"Black")
 message1.set_size(4.5)
-message2 = codesters.Text("I like my coffee Black :))",0,220,"Gold")
+message2 = codesters.Text("90% of the time I want to sleep",0,-220,"Gold")
+

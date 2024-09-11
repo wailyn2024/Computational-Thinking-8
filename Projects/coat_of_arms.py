82add07fb1982272d32f8a76d9e6864971d9da9c
lachlannnnnn <lachlanforrest@seattleacademy.org>
2024-09-11 21:48:08 +0000

    project 1

--- Projects/coat_of_arms.py
+++ Projects/coat_of_arms.py
@@ -9,11 +9,20 @@ stage = StageClass()
 stage.set_background("winter")
 
 
-s1 = codesters.Square(100, 100, 200, 'darkgreen')
+s1 = codesters.Square(100, 100, 200, 'forestgreen')
 s2 = codesters.Square(-100, 100, 200, 'mediumturquoise')
 s3 = codesters.Square(-100, -100, 200, 'limegreen')
-s4 = codesters.Square(100, -100, 200, 'mediumblue')
+s4 = codesters.Square(100, -100, 200, 'deepskyblue')
 
 
-sp1 = codesters.Sprite("pinetree", 100, 100)
+sp1 = codesters.Sprite("tree", 100, 100)
+sp1.set_size(0.8)
 sp2 = codesters.Sprite("fishingrod", -100, 100)
+sp2.set_size(0.2)
+sp3 = codesters.Sprite("sailboat", 100, -100)
+sp4 = codesters.Sprite("sportsballs", -100, -100)
+sp4.set_size(0.8)
+
+
+message1 = codesters.Text("Lachlan Forrest",0,220,"black")
+message2 = codesters.Text("Eat your frog",0,-220)

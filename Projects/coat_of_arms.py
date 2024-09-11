9c21fa4248aaa14e48b9fe025c1477b2cfe9ac26
gideonSAAS <gideonsanders@seattleacademy.org>
2024-09-11 21:45:51 +0000

    finished project 1

--- Projects/coat_of_arms.py
+++ Projects/coat_of_arms.py
@@ -13,4 +13,9 @@ q3 = codesters.Square(-100,-100,200,'Silver')
 q4 = codesters.Square(100,-100,200,'Gray') 
 
 S1 = codesters.Sprite("paw2",100,100)
-S2 = codesters.Sprite("basketball3",-100,-100)
+S2 = codesters.Sprite("basketball5",-100,-100)
+S3 = codesters.Sprite("videogames",-100,100)
+S4 = codesters.Sprite("bouldering",100,-100)          
+                    
+message1 = codesters.Text("Gideon Sanders",0,220,"black")
+message2 = codesters.Text("Games, Cats, Climbing, Basketball",0,-220,"white")

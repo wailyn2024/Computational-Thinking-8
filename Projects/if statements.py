847a8036a1dbf6a8b4fd12cb880be7764df4eda2
Astrojam8 <liamkudoking@seattleacademy.org>
2024-09-12 17:51:45 +0000

    grandma likes project

---
+++ Projects/if statements.py
@@ -0,0 +1,9 @@
+while True:
+    word = input("what do you think grandma like? ")
+
+    if "z" in word.lower():
+        print(f"Grandma doesn't like {word}!")
+    else:
+        print(f"grandma likes {word}!")
+
+    print(" ")

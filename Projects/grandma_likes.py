71aac9492dbcaf90e846e3474ab920583e1cefce
zoe-smith-SAAS <zoesmith@seattleacademy.org>
2024-09-13 16:22:30 +0000

    save

---
+++ Projects/grandma_likes.py
@@ -0,0 +1,9 @@
+while True:
+    word = input("What do you think grandma likes?")
+
+    if "g" in word or "r" in word or "a" in word or "n" in word or "d" in word or "m" in word:
+        print(f"Grandma likes {word}!")
+    else:
+        print(f"Grandma doesn't like {word}")
+
+    print("does grandma like it?")

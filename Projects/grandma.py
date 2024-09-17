fd30ca52c8e4a6ceb222d6ccf4a9f58c757b1a77
june121510 <junewyckoff@seattleacademy.org>
2024-09-17 17:06:55 +0000

    Porject 1 submission

---
+++ Projects/grandma.py
@@ -0,0 +1,9 @@
+while True:
+    word = input("What do you think grandma likes? ")
+
+    if :
+        print(f"Grandma doesn't like that word")
+
+    else:
+        print (f"Grandma likes that word!")
+      

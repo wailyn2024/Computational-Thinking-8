0fc0e2a94c5c8bebdde6578ace96f30715984a37
zoe-smith-SAAS <zoesmith@seattleacademy.org>
2024-09-17 21:52:20 +0000

    save

---
+++ Projects/9/17code.py
@@ -0,0 +1,9 @@
+while True:
+    word = input ("What foods do you think I like?")
+
+    if "i" in word or "p" in word or "t" in word:
+        print(f"I don't like it")
+    else:
+        print(f"I like it!")
+
+        print("")

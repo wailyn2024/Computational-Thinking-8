0c27b0af40ea276d908aad45c59d23a0c2aa79cf
RetroSeason <morganbesse@seattleacademy.org>
2024-09-18 17:50:59 +0000

    work

---
+++ Projects/grandmalike.py
@@ -0,0 +1,16 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
+
+
+while True:
+    word =input("What do you think Grandma likes?")
+    if "a" or "b" in word.lower:
+        print(f"Grandma loves {word}!")
+    else:
+        print(f"Grandma doesn't like {word}...")

8155150146cd6cf5d97230aec13840594a26493c
Jamari716 <Jamariross@seattleacademy.org>
2024-09-18 17:51:13 +0000

    prodject2

--- Projects/project_1.py
+++ Projects/project_1.py
@@ -6,14 +6,6 @@ stage = StageClass()
 ###############################################
 
 
-
-stage.set_background("rainstorm")
-
-sprite1 = codesters.sprite("drum",100,100)
-sprite2 = codesters.sprite("sailboat",100,-100)
-sprite3 = codesters.sprite("bass",-100,100)
-sprite4 = codesters.sprite("seattle",-100,-100)
-
 square1 = codesters.sprite("red",100,-100)
 square2 = codesters.sprite("purple",100,100) 
 square3 = codesters.sprite("white",-100,100)

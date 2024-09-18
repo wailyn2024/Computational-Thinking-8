d7afbaf754f8e920d213815804f0ef58d2a6eb3e
MelatiChickering <melatichickering@seattleacademy.org>
2024-09-18 17:50:46 +0000

    start project 2

---
+++ Projects/protoject2.py
@@ -0,0 +1,22 @@
+#intro
+print("you walk into your local mall")
+print("You then realize that no one is there")
+print("the door closes behind you and locks")
+print("you here a growling noise coming from somewhere near by")
+print("can you survive the apocalypse and escape?")
+
+#choice 1
+c1= input("do you ride the [elevator] or walk towards the [growling]?")
+if "elevator" in c1:
+    print("you press the button and enter the elevator")
+else:
+    print("you slowly walk towards the growling")
+
+#choice 2
+c2=input("you exit onto the fifth floor and see a long [hallway], do you walk down it or enter the room to your [right]?")
+if "hallway" in c2:
+    print("you walk down the hallway")
+else:
+    print("you enter the room and a zombie bites you on the arm")
+
+    

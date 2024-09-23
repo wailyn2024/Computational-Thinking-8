a3621005eb75812f20dababc00ffe0e877630197
MikhailAlexander <Mishauslontsev@seattleacademy.org>
2024-09-23 21:55:22 +0000

    Project BlackJack

---
+++ Projects/grandma.py
@@ -0,0 +1,9 @@
+while True:
+    word = input("what do you think grandma likes?")
+
+    if "e" in word:
+        print (f"Grandma doesn't like{word}!")
+    else:
+        print (f"Grandma likes{word}")
+
+    print ("")

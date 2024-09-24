99662d71e2ed0eb1a566c8f0f9e326cf65fb003c
Emimae <emimaeyamadamorigeau@seattleacademy.org>
2024-09-24 17:01:26 +0000

    Text Adventure.py

--- Projects/Text Adventure.py
+++ Projects/Text Adventure.py
@@ -1,5 +1,5 @@
 # intro
-print("After years of work, you finally, come up with the potion that can turn anyone into a cat or a bunny with just one click of a botten ")
+print("After years of work, you finally come up with the potion that can turn anyone into a cat or a bunny with just one click of a botten ")
 print("But before you can produce it, you have to test it on yourself")
 print("")
 print("you take a sip of it, and start seeing stars.")
@@ -13,8 +13,10 @@ if "bunny" in c1:
     # choice 2
     print ("you are starting to get a little bit hungry.")
     c2 = input ("Do you want to eat [carrots] or [lettuce] or [cilantro]?")
-    if "carrots" in c2 or "lettuce" in c2:
+    if "carrots" in c2:
         print ("Yum! that was so good!")
+    elif "lettuce" in c2:
+        ("Yumm you love lettuce!")
     else:
         print("yuck! you find out you dont like cilantro ")
 
@@ -22,3 +24,32 @@ if "bunny" in c1:
 
 else:
     print(" nice! you turn into a baby kitten. Now you are a little tired")
+
+    c1 = input ("Do you want to fall asleep in a [meadow] or a [tree] or a [Bush]")
+    if "tree" in c1:
+        print ("Ouch! you fell out and hurt you paw!")
+        print ("Now you are stuck and dont know what to do.")
+        print ("There is something running in the distance, do you try and [hobble over] to it or [stay put]?")
+        if "stay put" in c1:
+            print("unfortunately you die because you were not able to make yourself go through the pain and try and find food.")
+        else: 
+            print ("The thing you saw was a human and gave you food to help you heal more quickly!")
+            print ("you were able to heal quickly and and keep thriving!")
+        
+    else:
+        print ("Hah,You woke up feeling well rested!")
+        # choice 3
+        c1 = input ("There is something running in the distance, do you try and [hobble over] to it or [stay put]?")
+        if "stay put" in c1:
+            print("unfortunately you die because you were not able to find food fast enough.")
+        else: 
+            print ("The thing you saw was a human and gave you food to help you heal more quickly!")
+            print ("you were able to heal quickly and and keep thriving!")
+        print ( "did you enjoy this experience of being an animal [yes] or [no]"?)
+               
+        if "yes" in c3
+        print ("Now you get to mass produce the potion!")
+        else:
+            print ("womp womp")
+        
+       

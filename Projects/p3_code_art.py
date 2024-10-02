260211f445416a0a5a528685e53e17bc4ce8a3e3
RetroSeason <morganbesse@seattleacademy.org>
2024-10-02 19:01:45 +0000

    p3_code_art_final

--- Projects/p3_code_art.py
+++ Projects/p3_code_art.py
@@ -11,9 +11,9 @@ t.speed(110)
 t.penup()
 t.goto(0,0)
 t.pendown()
-color = ["lavender", "Thistle", "plum","MediumPurple","MediumSlateBlue"]
+colors = ["lavender", "Thistle", "plum","MediumPurple","MediumSlateBlue"]
 for i in range(1000):
-    t.color(color%5)
+    t.color( colors[i%5])
     t.forward(10 + i)
     t.left(120)
 
@@ -23,16 +23,54 @@ t.speed(110)
 t.penup()
 t.goto(0,0)
 t.pendown()
-colors = ["blue", "teal", "indigo", "cyan"]
+colors = ["blue","teal","indigo","cyan"]
 for i in range (1000):
     t.color( colors[i%4])
     t.forward(10)
     t.left(61 + i)
 
 
+#SPIRAL TWO, OTHER DIRECTION
+t = turtle.Turtle()
+t.speed(110)
+t.setheading(0)
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+colors = ["blue","teal","indigo","cyan"]
+for i in range (1000):
+    t.color( colors[i%4])
+    t.forward(10)
+    t.left(122 + i)
+#SPIRAL 3
+t = turtle.Turtle()
+t.speed(110)
+t.setheading(0)
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+colors = ["blue","teal","indigo","cyan"]
+for i in range (1000):
+    t.color( colors[i%4])
+    t.forward(10)
+    t.left(-122 + i)
+#SPIRAL 4
+t = turtle.Turtle()
+t.speed(110)
+t.setheading(0)
+t.penup()
+t.goto(0,0)
+t.pendown()
+colors = ["blue","teal","indigo","cyan"]
+
+for i in range (1000):
+    t.color( colors[i%4])
+    t.forward(10)
+    t.left(-61 + i)
 
 #DO NOT TOUCH
 ######ENDING####
 #################
-t.exitonclick()
 #############

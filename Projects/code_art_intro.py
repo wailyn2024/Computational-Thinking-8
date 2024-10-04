b4baadb11d71b16d55740e8715984736325ccd35
zoe-smith-SAAS <zoesmith@seattleacademy.org>
2024-10-04 21:34:43 +0000

    !!!

---
+++ Projects/code_art_intro.py
@@ -0,0 +1,152 @@
+# ###############################################
+# ### SETUP ###
+import turtle
+# ###############################################
+
+t = turtle.Turtle()
+t.penup()
+t.goto(90, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(75, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(60, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(45, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(30, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(15, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(0, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(-15, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(-30, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.penup()
+t.goto(-45, -100)
+t.color("green")
+t.pendown()
+
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+
+
+# ###############################################
+# ### ENDING ###
+turtle.exitonclick()
+# ###############################################

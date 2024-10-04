6e9c1de26b9521b916424e7c19e390285fcb5c4f
aaditpurohit <aaditpurohit@seattleacademy.org>
2024-10-04 21:11:02 +0000

    drawingproject3

---
+++ Projects/11turtle.py
@@ -0,0 +1,48 @@
+import turtle,random,math
+t = turtle.Turtle()
+t.speed(200)
+t.penup()
+t.goto(-100, -100)
+t.color("red")
+t.pendown()
+turtle.bgcolor("Blue")
+colors = ["green", "Red"]
+for i in range(100):
+    t.color(random.choice(colors))
+    t.forward(120 + i)
+    t.left(72)
+
+t.goto(-100,-100)
+for i in range(120):
+    t.color(random.choice(colors))
+    t.forward(120 - i)
+    t.left(72)
+
+for i in range(100):
+    r = 50
+    t.circle(r + i)
+
+for i in range(100):
+    r = 50
+    t.circle(r - i)
+    t.circle(r + i)
+
+t.goto(-100,-100)
+for i in range(120):
+    t.color(random.choice(colors))
+    t.forward(120 - i)
+    t.right(90)
+t.goto(-100,-100)
+for i in range(100):
+    r = 41
+    t.circle(r + i)
+for i in range(100):
+    r = 41
+    t.circle(r - i)
+
+
+
+# ###############################################
+# ### ENDING ###
+turtle.exitonclick()
+# ###############################################

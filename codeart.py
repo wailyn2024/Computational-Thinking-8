d5d2f991be61363855b722e4952a51daaad610e3
ayanaa122910 <ayanatrinide@seattleacademy.org>
2024-10-07 17:18:53 +0000

    ayana trinide

---
+++ codeart.py
@@ -0,0 +1,31 @@
+import turtle
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("pink")
+turtle.bgcolor("black")
+t.pendown()
+
+for i in range (6):
+    t.forward(100)
+    t.left(60)
+
+for i in range (30):
+    t.forward(160)
+    t.left(100 +1)
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-10, -100)
+t.color("red")
+t.pendown()
+
+for i in range (3):
+    t.forward(100)
+    t.left(120)
+for i in range (30):
+    t.forward(160)
+    t.left(120 +1)
+
+turtle.exitonclick()

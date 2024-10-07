a9477077a5590a4d173ad23b251bcef43755a85f
Jamari716 <Jamariross@seattleacademy.org>
2024-10-07 17:12:43 +0000

    project 3 submission

--- Projects/p3.py
+++ Projects/p3.py
@@ -1,17 +1,31 @@
+#set up
 import turtle
-
-t = turtle
-t.speed(100)
-t.goto(-100, -100)
-t.color("cyan")
-for i in range(100):
-    t.forward(100 + i)
-    t.left(120 + 1)
+#turtle1
+t = turtle.Turtle()
+t.speed(100) 
 t.penup
-t.goto(-75, -75)
+t.goto(0,0)
 t.pendown
-t.color("red")
+t.color("cyan")
+for i in range(150):
+    t.forward(100 + i)
+    t.left(120)
+#turtle2
+t2 = turtle.Turtle()
+t2.goto (0,100)
+t2.color("red")
+t2.speed(100)
 for i in range(100):
-    t.forward (100 + i)
-    t.right(72 +1)
+    t2.forward (100 + 10)
+    t2.left(90 +1)
+    #turtle 
+t3 = turtle.Turtle()
+t3.goto(0,-100)
+t3.speed(100)
+t3.color("red")
+for i in range (100):
+    t3.color("red")
+    t3.forward (100 + 10)
+    t3.left(90+ 1)
+
 turtle.exitonclick()

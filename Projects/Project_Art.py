5badcaed4617ee68ff5b455c65eb9836214ed643
H1ImL0st <divitgupta@seattleacademy.org>
2024-10-07 17:21:05 +0000

    Finish

--- Projects/Project_Art.py
+++ Projects/Project_Art.py
@@ -1,14 +1,28 @@
+#set up
 import turtle, random
+#variables
+colors = ["orchid","teal","cyan"]
+colors2 = ["indianred","salmon", "coral", "lightcoral"]
+background = ["gold", "magenta"]
+#turtle start
 t = turtle.Turtle()
 t.penup()
 t.goto(0,0)
 t.pendown()
-colors = ["orchid","teal","indian red","wheat"]
-for i in range(10000):
+#the object
+for i in range(500):
+    turtle.bgcolor(random.choice(background))
     t.color(random.choice(colors))
     t.speed(100)
-    t.forward(100+1)
-    t.left(150+i)
-turtle.exitonclick
-    
-
+    t.forward(50+i)
+    t.left(120+1)
+t.penup()
+t.goto( 0, 0)
+t.pendown()
+for i in range(400):
+    turtle.bgcolor(random.choice(background))
+    t.forward(50+i)
+    t.left(144+1)
+    t.color(random.choice(colors2))
+turtle.bgcolor("navy")
+turtle.exitonclick()

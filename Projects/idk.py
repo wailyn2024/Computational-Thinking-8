b55a40dc41cfa857822e247f32dfab3bdd2d6a20
mickilovesfood <mickinordstrom@seattleacademy.org>
2024-10-07 01:19:29 +0000

    coding art

--- Projects/idk.py
+++ Projects/idk.py
@@ -15,8 +15,17 @@ print("-----------------------------")
 # choice 1
 c1 = input ("do you choose to search in the [boats basement] or the [captains quarters]?")
 if "boats basement" in c1:
-    print("the old stairs break and you drown to death...")
-else:
-    print("the door to the captains quarters opens and you see the dark gloomy room full of chests!") 
+     print("the door to the captains quarters opens and you see the dark gloomy room full of chests!")    
+    # choice 2
+c1 = input ("do you open the [small shiny chest] or the [big rusty chest]?")
 
 
+if "small shiny chest" in c1:
+    print("a booby trap catches you as you walk there and it cuts off your head...")
+else: 
+    print ("the chest creeks slowly open and inside it holds gold coins and precious gems")
+print("the old stairs break and you drown to death...")
+
+
+# choice 3
+c1 = input ("")

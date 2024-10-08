a18cdca556f6b12edf14a01f23915fd274d489fb
aridadhiasaas <aridadhia@seattleacademy.org>
2024-10-08 15:47:59 +0000

    project 3 submission Ari

--- Projects/project3.py
+++ Projects/project3.py
@@ -4,10 +4,22 @@ t = turtle.Turtle()
 t.speed(150)
 turtle.bgcolor("red")
 
-
+colors = ["gray", "yellow", "purple", "white", "black"]
 for i in range(100):
     t.color("blue")
     t.forward(50 + i)
-    t.left(25)
+    t.left(75)
 
+
+for i in range(500):
+    t.color(random.choice(colors))
+    t.forward(100 + i%100)
+    t.left(120)
+
+colors2 = ["blue", "yellow", "red", "orange", "brown" ]
+for i in range(250):
+    t.goto(200,200)
+    t.color( colors [ i % 3 ] )
+    t.forward(70)
+    t.left(175)
 turtle.exitonclick()

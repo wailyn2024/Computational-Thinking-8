434cac066a2acabe680192c8fe0e2d48ec6bc07e
lidapida <lydiaryvkin@seattleacademy.org>
2024-10-08 16:18:28 +0000

    project3.py

--- Projects/project3.py
+++ Projects/project3.py
@@ -1,14 +1,15 @@
 import turtle, random, math
-t= turtle.Turtle()
-
-t.speed(50)
-turtle.bgcolor("pink")
-
-colors = ["light yellow" , "dark salmon"]
-for i in range (2000):
-    t.color(random.choice(colors))
-    t.forward (100 + i%50)
-    t.left(random.choice([90,95,92]))
-
-
-
+t = turtle.Turtle()
+#speed (300)
+t.goto(100, 50)
+t.color("pink")
+#colors
+colors = ["black","purple","pink","yellow","red"]
+for i in range(10000):
+#shape
+    t.color (random.choice(colors))
+    t.forward(100+i%50)
+    t.right (45)
+    t.left (10)
+turtle.exitonclick() 
+#i have so much colors in my shape 

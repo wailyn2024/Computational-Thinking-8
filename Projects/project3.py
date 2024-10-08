6ff2b7e38bf24ca02ba4f75a3749cdd514634d59
keeganbenisa <keeganbenisa@seattleacademy.org>
2024-10-08 15:51:53 +0000

    project 3

---
+++ Projects/project3.py
@@ -0,0 +1,15 @@
+import turtle, random, math
+t= turtle.Turtle()
+#speed
+t.speed(100)
+turtle.bgcolor("purple")
+#colors
+colors = ["Navy", "RoyalBlue"]
+for i in range(20000):
+    t.color(random.choice(colors))
+    t.forward(150+i%15)
+#shape
+    t.left (120)
+
+    
+turtle.exitonclick()

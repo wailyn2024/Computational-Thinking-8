79559dd505b07c42eb1303eee4390852ab230370
aridadhiasaas <aridadhia@seattleacademy.org>
2024-10-08 16:19:37 +0000

    project 2 resubbmisiion(adding if statements)

--- Projects/project3.py
+++ Projects/project3.py
@@ -1,6 +1,6 @@
 import turtle,random, math
 t = turtle.Turtle()
-
+#changes turtle's pen
 t.speed(150)
 turtle.bgcolor("red")
 
@@ -10,13 +10,14 @@ for i in range(100):
     t.forward(50 + i)
     t.left(75)
 
-
+# draws first shape
 for i in range(500):
     t.color(random.choice(colors))
     t.forward(100 + i%100)
     t.left(120)
 
 colors2 = ["blue", "yellow", "red", "orange", "brown" ]
+# draws next shape
 for i in range(250):
     t.goto(200,200)
     t.color( colors [ i % 3 ] )
--- Projects/text_adventure.py
+++ Projects/text_adventure.py
@@ -23,8 +23,24 @@ if "deck" in c1:
             print("You spin the wheel revealing a huge key......")
         elif "board" in c3:
             print("You investigate the board.")
+            c5 = input("Do you move on or play with the board?")
+            if "investigate" or "board" in c5:
+                print("You slide the board away revevealing a picture")
+            else:
+                print("You  move on")
         else:
             print("You walk towards the rug.")
+            c4 = input("Do you want to uncover the rug or move on")
+            if "rug" or "uncover" in c4:
+                print("You slide away the rug revealing a trap door")
+                c6 = input("Do you want to investigate the trap door")
+                if "yes" or "trap" or "investigate" in c6:
+                    print("You open the trap door revialing a ominous basement")
+                else:
+                    print("You move on and leave")
+            else:
+                print("You move on and go back to shore.")
+                print("Game over")
     else:
         print("You walk right to the bathroom.")
 else:

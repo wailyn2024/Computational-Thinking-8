9e27627184a5884fd469a028fb7bf42386642aaf
braylen-333 <braylenbrooks@seattleacademy.org>
2024-10-08 15:51:55 +0000

    resumbit

--- adventure.py
+++ adventure.py
@@ -23,9 +23,33 @@ if "5th" in c1:
     print("  ")
     print("Except for one...")
     c2 = input("Do you go down to the [4th floor] or [into the unlocked class room] ?")
-    if "4th" in c2 :
-        print("You walk down to the 4th floor")
+    if "class" in c2 :
+        print("you walk into 503")
 
+# choice 3
+        c1 = input ("You find an open computer, displaying a game. do you [play] or do you [walk away]? ")
+if "play" in c1:
+        print("you press start")
+
+# choice 4 
+        c7 = print ( "You arrive at school" )
+print ("")
+print ( "Strangely ... no one is there" )
+print ( " " )
+print ( "Can you figure out whats going on?" )
+print ( " " )
+print ( " " )
+print ( "✦•┈๑⋅⋯🎃⋯⋅๑┈•✦ ✦•┈๑⋅⋯🎃⋯⋅๑┈•✦ ✦•┈๑⋅⋯🎃⋯⋅๑┈•✦" )
+print ( " " )
+print ( " " )
+
+# choice 1 
+c1 = input ("Would you like to go to [the 3rd floor], [the 4th floor], [or the 5th floor] ?")
+if "5th" in c1: 
+    print ("You walk up to the 5th floor")
+
+else:
+        print("you walk to the 4th floor")
 # choice 7
 c3 = input ("you notice a letter on the couch, do you [open it] or [leave it there] ? ")
 if "open" in c3:
@@ -37,14 +61,13 @@ if "open" in c3:
 
 else:
         print("you walk to the 3rd floor")
-        
-    else:
-        print("you walk into 503")
+
+
 
 # choice 3
         c3 = input ("You find an open computer, displaying a game. do you [play] or do you [walk away]? ")
 if "play" in c3:
-        print("you press start")
+        print("you press start...")
 
 # choice 4 
         c7 = print ( "You arrive at school" )
@@ -65,8 +88,8 @@ if "5th" in c1:
 
 
 
-elif "4th" in c1:
-      print("you walk to the 4th floor")
+elif "3rd" in c1:
+      print("you walk to the 3rd floor")
 
 
 
@@ -89,7 +112,11 @@ if "leave" in c4:
       print("smart choice")
       print("Safe ending")
 else:
-        print("you walk past it and go downstairs")
+        print("you decide to stay!")
+        print("And now you can be apart of the surprise party too!")
+        print("  ")
+        print("forever...")
+        print("Stuck ending")
 
 
 

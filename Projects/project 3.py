472bae7399d437a512016a6d9b356aba8d73b9b8
tmoney1313 <tabithamadden@seattleacademy.org>
2024-10-08 15:58:36 +0000

    project 3

---
+++ Projects/project 3.py
@@ -0,0 +1,26 @@
+import turtle
+t = turtle.Turtle()
+
+t.goto(100, 0)
+#color change 
+t.color("red")
+
+for i in range (200):
+    t.forward(100 + i)
+    #switch direction
+    t.left(72)
+
+
+for i in range (200):
+    t.forward(110 + i)
+    t.left (90)
+
+
+for i in range (300):
+    t.speed(400 + i)
+    #speedup
+    t.left (88)
+turtle. exitonclick()
+
+
+

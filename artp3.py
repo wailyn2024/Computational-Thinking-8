ed518cfd5a2737e11d074935d648e704a84e19cc
Quinn151 <quinnkoester@seattleacademy.org>
2024-10-09 18:25:19 +0000

    project 3

--- artp3.py
+++ artp3.py
@@ -6,13 +6,16 @@ t = turtle.Turtle()
 turtle.bgcolor("pink")
 t.speed(100)
 
+#colors 
 colors = ["yellow","white","orange"]
 angles = [50]
 sides = [45, 78, 90]
 
+#design 
 for i in range(1000): 
     t.color( colors[i%3])
     t.forward(100 + 1 + i)
     t.left(45 + 1)
 
+#end
 turtle.exitonclick()

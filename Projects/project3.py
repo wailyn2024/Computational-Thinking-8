61b1d125ad6da348f6baed1b9da438ddf40db25a
kaicanhoop <kaisoga@seattleacademy.org>
2024-10-16 15:38:14 +0000

    project3

--- Projects/project3.py
+++ Projects/project3.py
@@ -11,8 +11,10 @@ for i in range(500) :
     t.forward(100+i)
     t.left(100+3)
     t.color("red")
+    # on line 13 ir make my line red
     t.forward(100+i)
     t.left(100+8)
     t.color("DeepSkyBlue")
 
 turtle.exitonclick()
+#on line 11 it make i
---
+++ Projects/wordle.py
@@ -0,0 +1,57 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+import random
+
+# Pick a word at random
+word_list = ["loopy","heart","audio","laugh","trial"]
+hidden_word = random.choice(word_list)
+
+# Repeat for 6 guesses
+for i in range(6):
+    # Guess a word
+    guess_word = input()
+    output = ""
+
+    # First letter (in python, counting starts at 0 not 1)
+    if guess_word[0] == hidden_word[0]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+    
+
+    # Result
+    print(output)
+    if output == "🟩🟩🟩🟩🟩":
+        print("You win")
+        break
+
+print(f"You used {i+1} guesses")
+

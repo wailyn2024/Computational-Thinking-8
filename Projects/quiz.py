16471ad597af0d122a2e071ef6cd80e53c55a05b
lachlannnnnn <lachlanforrest@seattleacademy.org>
2024-10-18 21:16:22 +0000

    proj 4

--- Projects/quiz.py
+++ Projects/quiz.py
@@ -1,34 +1,55 @@
+#Beginning
 cat_points = 0
 dog_points = 0
+fish_Points = 0
 
+#Middle
 # question 1:
-answer = input("On a weekend would you rather A) nap all day, or B) go on a hike?")
+answer = input("On a weekend would you rather A) nap all day, B) go on a hike, or C) Go on a nice walk?")
 if answer == "A":
 	cat_points += 1
 elif answer == "B":
 	dog_points += 1
+elif answer == "C":
+	fish_Points += 1
 
 # question 2:
-answer = input("Are you A) an extrovert, or B) an introvert?")
+answer = input("Are you A) an extrovert, B) an introvert or C) a bit of both?")
 if answer == "A":
 	dog_points += 1
 elif answer == "B":
 	cat_points += 1
+elif answer == "C":
+	fish_Points += 1
 
 # question 3:
-answer = input("Would you rather eat lunch A) by yourself, or B) with a group?")
+answer = input("Would you rather eat lunch A) by yourself, B) with a group or C) with one friend?")
 if answer == "A":
 	cat_points += 1
 elif answer == "B":
 	dog_points += 1
+elif answer == "C":
+	fish_Points += 1
 
 #question 4
-answer = input("do you like, A) halloween or B) christmas ")
+answer = input("do you like, A) halloween B) christmas or C) 4th of july more?")
 if answer == "A":
 	dog_points += 1
 elif answer == "B": 
     cat_points += 1
-	
+elif answer == "C":
+	fish_Points += 1
+
+#Question 5
+answer = input("do you prefer A) sitting B) standing, or C) crouching more?")
+if answer == "B":
+	dog_points += 1
+elif answer == "A":
+	cat_points += 1
+elif answer == "C":
+	fish_Points += 1
+
+#End
 #end of the quiz
 if cat_points > dog_points:
 	print("You are a cat person")

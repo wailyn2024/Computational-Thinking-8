016ef27d9a7c508a425b84bf23c35f630951ee00
lachlannnnnn <lachlanforrest@seattleacademy.org>
2024-10-18 21:31:59 +0000

    proj 4

--- Projects/quiz.py
+++ Projects/quiz.py
@@ -50,8 +50,13 @@ elif answer == "C":
 	fish_Points += 1
 
 #End
-#end of the quiz
 if cat_points > dog_points:
+	if cat_points > fish_Points:
 	print("You are a cat person")
+	elif fish_Points > cat_points
+	Print("you are a fish person!")
 elif dog_points > cat_points:
+	if dog_points > fish_Points:
 	print("You are a dog person")
+	elif dog_points < fish_Points:
+	print("You are a fish person")

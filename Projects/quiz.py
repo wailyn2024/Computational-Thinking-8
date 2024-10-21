5fe409081894aa438ddff575ed81f01aa814ddf4
MelatiChickering <melatichickering@seattleacademy.org>
2024-10-21 17:52:02 +0000

    personality quiz submission

---
+++ Projects/quiz.py
@@ -0,0 +1,60 @@
+#Beginning
+Paris_points = 0
+Rory_points = 0
+Lane_points=0
+
+#Middle
+
+# question 1:
+answer = input("On a weekend would you rather A) Study, or B) Extra school activity, or C)Listen to music?")
+if answer == "A":
+	Rory_points += 1
+elif answer == "B":
+	Paris_points += 1
+elif answer =="C"
+	Lane_points += 1
+
+# question 2:
+answer = input("How would you describe your style A) Downtown girl/academia, or B) preppy/coquette, C) retro/vintage?")
+if answer == "A":
+	Rory_points += 1
+elif answer == "B":
+	Paris_points += 1
+elif answer == "C" +=1
+	Lane_points += 1
+
+# question 3:
+answer = input("Would you rather eat lunch A) by yourself, or B) with a big group, or C) with just your best friends?")
+if answer == "A":
+	Rory_points += 1
+elif answer == "B":
+	Paris_points += 1
+elif answer == "C" += 1
+	Lane_points += 1
+	
+
+# question 4
+answer = input("Would you rather A) do an at home workout, or B) do pilates, or C) go for a run?")
+if answer == "A":
+	Rory_points += 1
+elif answer == "B"
+	Paris_points += 1
+elif answer == "C" += 1
+	Lane_points += 1
+
+#question 5
+answer = input("Finally whats your favorite flower?,  A) Roses, or B) Lily's, or C) Sunflower")
+if answer == "A":
+	Rory_points += 1
+elif answer == "B"
+	Paris_points += 1
+elif answer == "C" += 1
+	Lane_points += 1
+ 
+ # end of quiz:
+if Rory_points > Paris_points and Rory_points > Lane_points:
+	print("You are most alike to Rory!")
+elif Paris_points > Rory_points and Paris_points > Lane_points:
+	print("You are most like Paris!")
+elif Lane_points > Rory_points and Lane_points > Paris_points
+`   qf2`

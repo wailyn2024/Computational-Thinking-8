3518f227c85616fcadd1ed12466cf9b1916f8e66
MelatiChickering <melatichickering@seattleacademy.org>
2024-10-23 19:05:04 +0000

    Personality quiz project submisssion, fixed version

--- Projects/quiz.py
+++ Projects/quiz.py
@@ -11,7 +11,7 @@ if answer == "A":
 	Rory_points += 1
 elif answer == "B":
 	Paris_points += 1
-elif answer =="C"
+elif answer =="C":
 	Lane_points += 1
 
 # question 2:
@@ -20,7 +20,7 @@ if answer == "A":
 	Rory_points += 1
 elif answer == "B":
 	Paris_points += 1
-elif answer == "C" +=1
+elif answer == "C":
 	Lane_points += 1
 
 # question 3:
@@ -29,7 +29,7 @@ if answer == "A":
 	Rory_points += 1
 elif answer == "B":
 	Paris_points += 1
-elif answer == "C" += 1
+elif answer == "C":
 	Lane_points += 1
 	
 
@@ -37,18 +37,18 @@ elif answer == "C" += 1
 answer = input("Would you rather A) do an at home workout, or B) do pilates, or C) go for a run?")
 if answer == "A":
 	Rory_points += 1
-elif answer == "B"
+elif answer == "B":
 	Paris_points += 1
-elif answer == "C" += 1
+elif answer == "C":
 	Lane_points += 1
 
 #question 5
 answer = input("Finally whats your favorite flower?,  A) Roses, or B) Lily's, or C) Sunflower")
 if answer == "A":
 	Rory_points += 1
-elif answer == "B"
+elif answer == "B":
 	Paris_points += 1
-elif answer == "C" += 1
+elif answer == "C":
 	Lane_points += 1
  
  # end of quiz:
@@ -56,5 +56,5 @@ if Rory_points > Paris_points and Rory_points > Lane_points:
 	print("You are most alike to Rory!")
 elif Paris_points > Rory_points and Paris_points > Lane_points:
 	print("You are most like Paris!")
-elif Lane_points > Rory_points and Lane_points > Paris_points
-`   qf2`
+elif Lane_points > Rory_points and Lane_points > Paris_points:
+	print("you are most alike to Lane!")

7ee86a691af2ed1fba24452657cc27724407eaba
MikhailAlexander <Mishauslontsev@seattleacademy.org>
2024-10-23 15:39:06 +0000

    Cool Art

--- Projects/Cool_Art_2.py
+++ Projects/Cool_Art_2.py
@@ -1,12 +1,15 @@
 import turtle
 import random
 
+#Set Backdrop
 screen = turtle.Screen()
 screen.bgcolor("black")
 
+#Set Speed
 t = turtle.Turtle()
 t.speed(0)
 
+#What To Draw
 def draw_spiral():
     for i in range(100):
         t.color(random.random(), random.random(), random.random())

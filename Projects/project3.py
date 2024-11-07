3037880a970e6df8558f4650f98ee7d0f095815f
blakecarandang <blakecarandang@seattleacademy.org>
2024-11-07 21:20:23 +0000

    coatofarms

--- Projects/project3.py
+++ Projects/project3.py
@@ -3,13 +3,11 @@ import turtle
 # Create a turtle object
 t = turtle.Turtle()
 
-# Set the turtle's position and color
+# Set the turtle's position and color to pink
 t.goto(100, 0)
-t.color('cyan')
+t.color('pink')
 
 # Draw a 5-sided star
 for i in range(5):
     t.forward(180)
     t.left(72)
-
-# Keep the turtle window open until clicked

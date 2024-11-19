f86551fcb1ada9700fa046b14a222bd850efa418
sheydaharvey <sheydaharvey@seattleacademy.org>
2024-11-19 22:14:00 +0000

    this it my dodge game

--- keegan&sheyda.py
+++ keegan&sheyda.py
@@ -1,10 +1,14 @@
 void setup() {
-    pinMode(13, OUTPUT)
+    pinMode(13, OUTPUT);
 }
 
 void loop() {
     digitalWrite(13, HIGH);
-    delay (1000);
+    delay (1000000);
     digitalWrite(13, LOW);
-    delay (1000);
+    delay (1000000);
+    digitalWrite(13, HIGH);
+    delay (25);
+    digitalWrite(13, LOW);
+    delay (25);
 }

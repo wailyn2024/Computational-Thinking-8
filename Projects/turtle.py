82c38813d341101f33fb6d68abe809a970521a7b
sheydaharvey <sheydaharvey@seattleacademy.org>
2024-11-20 20:02:05 +0000

    i re did my turtle project so it now says end game. -sheyda

--- Projects/turtle.py
+++ Projects/turtle.py
@@ -1,8 +1,9 @@
-# Section 1 - Setup
+# Section 1 -Setup
 import codesters, random
 from codesters import StageClass
 stage = StageClass()
 stage.set_background("winter")
+
 
 player = codesters.Sprite("fish")
 player.set_size(0.70)
@@ -12,57 +13,41 @@ stage.disable_floor()
 gameOver = False
 lives = 5
 
-# Section 2 - Objects
-
+#section 2 falling objects
 def falling_object():
-	global gameOver
-	if not gameOver:
-		x_position = random.randint(-250,250)
-		object = codesters.Sprite("soccerball", x_position, 250)
-		object.set_size(0.4)
-		object.set_y_speed(-10)
-    
+    global gameOver
+    if not gameOver:
+        x_position = random.randint(-250,250)
+        object = codesters.Sprite("soccerball", x_position, 250)
+        object.set_size(0.4)
+        object.set_y_speed(-10)
+   
 stage.event_interval(falling_object, 0.5)
 
-# Section 3 - Collision
 
-def collision(player, object):
-	global lives, gameOver
-    
-	if object.get_image_name() == "soccerball":
-		stage.remove_sprite(object)
-		if lives == 0:
-			print("test")
-		else:
-			print("test")
-         	 
+# Section 3 Collision
+def collision(s1, s2):
+    global lives, gameOver
+   
+    if s2.get_image_name() == "soccerball":
+        stage.remove_sprite(s2)
+        if lives == 0:
+            print("game over")
+            gameOver = True
+
+
+        else:
+            print("lose one life")
+            lives-= 1
+
+
 player.event_collision(collision)
 
-
 # Section 4 - Controls
-def move_up(sprite):
-    sprite.move_up(10)
-     
-def move_down(sprite):
-    sprite.move_down(10)
-    
 def move_left(sprite):
-    sprite.move_left(10)
+    sprite.move_left(5)
     
 def move_right(sprite):    
-    sprite.move_right(10)
-
-def turn_left(sprite):
-    heading = sprite.heading
-    sprite.set_heading(heading + 1)
-
-def turn_right(sprite):
-    heading = sprite.heading
-    sprite.set_heading(heading - 1)
-
-
-player.event_key("w", move_up)
-player.event_key("s", move_down)
+    sprite.move_right(5)
 player.event_key("a", move_left)
 player.event_key("d", move_right)
-

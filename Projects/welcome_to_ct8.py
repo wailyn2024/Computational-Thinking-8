673ca9cfd4319497a951306b633bb9de2cfdc83a
wailyn2024 <wailynchen@seattleacademy.org>
2024-12-04 20:13:07 +0000

    day 2 CT8

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -12,6 +12,9 @@ mySprite.say("I completed the task")
 
 
 
+
+
+
 print("\n\nWhen you have found the CARDINAL, click here, then use CTRL C to end the program\n\n")
 print("This is the new last instruction")
 mysprite= codesters. Sprite("baseball",-230,230)

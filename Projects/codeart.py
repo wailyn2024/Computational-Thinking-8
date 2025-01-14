67837c9e2c48229b393d2801f12a72fb5c42a39d
wailyn2024 <wailynchen@seattleacademy.org>
2025-01-14 18:35:49 +0000

    I'm finished

--- Projects/codeart.py
+++ Projects/codeart.py
@@ -1,15 +1,16 @@
+#Start
 import turtle 
 
 t= turtle.Turtle()
-
+#Middle 
 t.goto(100,0)
-colors= ["pink,yellow"]
-t.color ( colors[i % 2])
+colors= ["pink","yellow"]
 t.color("cyan")
 
 for i in range(5):
-
+    t.color ( colors[i % 2])
     t.forward(100)
     t.left(72)
 
 turtle.exitonclick()
+#End 
